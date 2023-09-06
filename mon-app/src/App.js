40d0ff2f76bb8c5553e3bf2f2e0e48feb1bd3fc8
Jun080 <m_dassonville3@hetic.eu>
2023-09-06 14:29:24 +0200

    Mail + CV buttons

--- mon-app/src/App.js
+++ mon-app/src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import img from './img/image-profil.jpg'
+import resume from './img/CV.pdf'
 import IconButton from './components/button.jsx'
-import { FaGithub, FaLinkedin, FaPaperclip } from 'react-icons/fa'; // Importez l'icône LinkedIn également si nécessaire
+import { FaGithub, FaLinkedin, FaPaperclip } from 'react-icons/fa';
+import { HiMail } from 'react-icons/hi';
 
 function App() {
   return (
@@ -12,8 +14,9 @@ function App() {
       <div className='mt-5 font-semibold'>@MorganeDassonville</div>
       <div className='my-3 flex flex-col'>
         <IconButton label="GitHub" icon={FaGithub} url={'https://github.com/Jun080'} />
-        <IconButton label="LinkedIn" icon={FaLinkedin} url={'https://github.com/Jun080'} />
-        <IconButton label="CV" icon={FaPaperclip} />
+        <IconButton label="LinkedIn" icon={FaLinkedin} url={'https://www.linkedin.com/in/morgane-dassonville/'} />
+        <IconButton label="CV" icon={FaPaperclip} url={resume} />
+        <IconButton label="Mail" icon={HiMail} url={'mailto:morganedassonville08@gmail.com'} />
       </div>
     </div>
   );
